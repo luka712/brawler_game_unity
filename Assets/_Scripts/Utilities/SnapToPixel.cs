ec640a02aef948f241771c195263fe706d03e173
luka712 <lukaa712@hotmail.com>
2018-02-07 22:57:23 +0100

    - snap to pixel

---
+++ Assets/_Scripts/Utilities/SnapToPixel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToPixel : MonoBehaviour
+{
+    public float pixelsPerUnit = 8;
+
+    private void LateUpdate()
+    {
+        transform.position = transform.position.ChangeComponentX
+            (Mathf.Round(transform.position.x * pixelsPerUnit) / pixelsPerUnit);
+
+        transform.position = transform.position.ChangeComponentY
+          (Mathf.Round(transform.position.y * pixelsPerUnit) / pixelsPerUnit);
+    }
+}
+
